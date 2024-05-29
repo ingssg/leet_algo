d5d51062527ecda8bfda509fe77880f33a800c61
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-05-30 00:17:41 +0900

    Time: 155 ms (71.50%), Space: 65.9 MB (31.59%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,14 +3,21 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    const ans = new Set();
+    const ans = new Array();
     nums.sort((a, b) => a-b)
     
     for(let i = 0; i < nums.length-2; i++) {
+      if(i > 0 && nums[i] === nums[i-1])
+        continue;
       let l = i + 1, r = nums.length - 1
       while(l<r) {
         if(nums[i] + nums[l] + nums[r] === 0){
-          ans.add(JSON.stringify([nums[i], nums[l], nums[r]]));
+          ans.push([nums[i], nums[l], nums[r]]);
+          while(l<r && nums[l] === nums[l+1])
+            l++;
+          while(l<r && nums[r] === nums[r-1])
+            r--;
+
           l++;
           r--;
         }
@@ -21,6 +28,5 @@ var threeSum = function(nums) {
           r--;
       }  
     }
-    const result = Array.from(ans).map(str => JSON.parse(str));
-    return result;
+    return ans;
 };
