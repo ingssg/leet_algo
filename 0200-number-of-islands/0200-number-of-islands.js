a1ecf0caa83373a063ece52541785470b9a882cc
KIM, IN-SEOK <80662781+ingssg@users.noreply.github.com>
2024-06-03 17:26:12 +0900

    Time: 2932 ms (5.01%), Space: 59.6 MB (23.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 
-const findLand = (grid, visited) => {
+const findLand = (grid) => {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] == 1 && !visited[i][j]) {
+      if (grid[i][j] == 1) {
         return [i, j];
       }
     }
@@ -21,29 +21,23 @@ const isValidRange = (e, rowEnd, colEnd) => {
 };
 
 var numIslands = function (grid) {
-  const visited = Array(grid.length)
-    .fill()
-    .map(() => Array(grid[0].length).fill(false));
-
   const q = [];
   let ans = 0;
   let isFirst = true;
   let newLand;
+  const diff = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
 
   while (
-    JSON.stringify((newLand = findLand(grid, visited))) !==
-    JSON.stringify([-1, -1])
+    JSON.stringify((newLand = findLand(grid))) !== JSON.stringify([-1, -1])
   ) {
     isFirst = false;
     q.push(newLand);
-    visited[newLand[0]][newLand[1]] = true;
-
-    const diff = [
-      [1, 0],
-      [-1, 0],
-      [0, 1],
-      [0, -1],
-    ];
+    grid[newLand[0]][newLand[1]] = 0;
 
     while (q.length > 0) {
       let s = q.shift();
@@ -51,11 +45,10 @@ var numIslands = function (grid) {
         let next = [s[0] + dif[0], s[1] + dif[1]];
         if (
           isValidRange(next, grid.length, grid[0].length) &&
-          !visited[next[0]][next[1]] &&
           grid[next[0]][next[1]] == 1
         ) {
           q.push(next);
-          visited[next[0]][next[1]] = true;
+          grid[next[0]][next[1]] = 0;
         }
       }
     }
